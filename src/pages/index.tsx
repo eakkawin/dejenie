6cef0cb8b6844d77183de1a0d8285c145ce3987d
Eakkawin <eakkawin.i@dv.co.th>
2021-01-16 21:22:15 +0700

    init page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "../css/style.css";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+    </main>
+  );
+};
+
+export default IndexPage;
